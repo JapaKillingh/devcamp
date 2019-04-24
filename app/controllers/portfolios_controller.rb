1d520aa53b01f98f6098dc40bbfe9125be87586c
JapaKillingh <willamis.japa391@gmail.com>
2019-04-24 18:09:28 -0400

    Implemented show functinality for

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,20 +19,24 @@ class PortfoliosController < ApplicationController
     end
   end
 
-def edit
+  def edit
     @portfolio_item = Portfolio.find(params[:id])
-end
+  end
 
-def update
-  @portfolio_item = Portfolio.find(params[:id])
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
 
-  respond_to do |format|
-    if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-      format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
-    else
-      format.html { render :edit }
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
   end
-end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
 end
