104b3490f66e00a5b3f5fe13375957be66919a1c
JapaKillingh <willamis.japa391@gmail.com>
2019-05-02 14:56:27 -0400

    toggle_status

--- db/migrate/20190429215526_add_blog_status_to_blogs.rb
+++ db/migrate/20190429215526_add_blog_status_to_blogs.rb
@@ -1,5 +1,5 @@
 class AddBlogStatusToBlogs < ActiveRecord::Migration[5.2]
   def change
-    add_column :blogs, :status, :intenger, default: 0
+    add_column :blogs, :status, :integer, default: 0
   end
 end
